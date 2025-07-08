297d7280c3560bc8a2d4b796aeb2a95d5a95bc54
DT <dd>
2025-07-08 13:23:01 +0200

    sekiro 8 endo

--- Client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,13 +1,16 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
+import ActivityFilter from "./ActivityFilter";
 
 export default function ActivityDashboard() {
   return (
     <Grid2 container spacing={3}>
-      <Grid2 size={7}>
+      <Grid2 size={8}>
         <ActivityList />
       </Grid2>
-      <Grid2 size={5}>Activity filters go here</Grid2>
+      <Grid2 size={4}>
+        <ActivityFilter />
+      </Grid2>
     </Grid2>
   );
 }
